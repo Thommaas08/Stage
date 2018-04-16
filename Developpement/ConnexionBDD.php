6d39569eceb3f7d3ce4285df28e0fb6c9923c1c5
Thomas Samul <thomas.samul@live.fr>
2018-04-16 17:28:29 +0200

    Ajout Addclient et modif du reste

--- Developpement/ConnexionBDD.php
+++ Developpement/ConnexionBDD.php
@@ -2,7 +2,7 @@
 try   //Connexion à la base de donnée avec message d'erreur
 {
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-    $bdd = new PDO('mysql:mysql-stage042018.alwaysdata.net;dbname=stage042018_cclo', '156947', 'Stage2018',$pdo_options);
+    $bdd = new PDO('mysql:mysql-stage042018.alwaysdata.net;dbname=stage042018_cclo', '156947_stage', 'STAGE2018',$pdo_options);
 }
 catch (Exception $e)
 {
