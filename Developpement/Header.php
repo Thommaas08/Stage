921d77c39cfa7c904acb18e38f8f692e7ead7983
Thomas Samul <thomas.samul@live.fr>
2018-04-18 17:29:55 +0200

    Modification footer header style css

--- Developpement/Header.php
+++ Developpement/Header.php
@@ -1,51 +1,49 @@
 <header>
-  <link href="css/StyleM.css" rel="stylesheet" type="text/css" />
-   <div class="headercontent">
-      <img src="img/Logo.png" alt="Logo">
-      <div class="navbar">
-         <ul id="nav">
-            <li class="current"><a href="#">Accueil</a></li>
-            <!-- n1 -->
-            <li>
-               <a href="#">Administration</a><!-- n1 -->
-               <ul>
-                  <li>
-                     <a href="#">Creation Menu <img src="images/multi.png" border="0" width="15" height="15" align="absmiddle"></a><!-- n2 -->
-                     <ul>
-                        <li><a href="#">Menu Classique</a></li>
-                        <!-- n3 -->
-                        <li><a href="#">Menu Sans Sel Ajouter</a></li>
-                        <!-- n3 -->
-                        <li><a href="#">Menu Sans Sucre Ajouter </a></li>
-                     </ul>
-                  </li>
-                  <li>
-                     <a href="#"> Commande <img src="images/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
-                     <ul>
-                        <li><a href="#">Créer une Commande</a></li>
-                        <!-- n3 -->
-                        <li><a href="#">Annuler une Commande</a></li>
-                        <!-- n3 -->
-                     </ul>
-                  </li>
-                  <li><a href="#">Feuille de route</a></li>
-                  <!-- n2 -->
-               </ul>
-            </li>
-            <li>
-               <a href="#">Bénéficiaires</a>
-               <ul>
-                  <li>
-                     <a href="#"> Créer un Bénéficiaire <img src="images/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a>
-                  </li>
-                  <li><a href="#">Supprimer un Bénéficiaire</a></li>
-                  <li><a href="#">Modifier un Bénéficiaire</a></li>
-               </ul>
-            </li>
-            <li><a href="#">Facturation</a></li>
-         </ul>
-         <br /><br /><br /><br /><br /><br />
-         <!--#include file=../footer_scr.asp-->
-      </div>
-   </div>
+   <!-- http://localhost/php/Developpement/ -->
+ <link href="css/StyleM.css" rel="stylesheet" type="text/css" />
+ <img src="img/logo.png" alt="Logo">
+ <ul id="nav">
+   <li class="current"><a href="index.php">Accueil</a></li>
+   <!-- n1 -->
+   <li>
+      <a href="#">Administration</a><!-- n1 -->
+      <ul>
+         <li>
+            <a href="#">Creation Menu </a><!-- n2 -->
+            <ul>
+               <li><a href="#">Menu Classique</a></li>
+               <!-- n3 -->
+               <li><a href="#">Menu Sans Sel Ajouter</a></li>
+               <!-- n3 -->
+               <li><a href="#">Menu Sans Sucre Ajouter </a></li>
+            </ul>
+         </li>
+         <li>
+            <a href="#"> Commande </a><!-- n2 -->
+            <ul>
+               <li><a href="#">Créer une Commande</a></li>
+               <!-- n3 -->
+               <li><a href="#">Annuler une Commande</a></li>
+               <!-- n3 -->
+            </ul>
+         </li>
+         <li><a href="#">Feuille de route</a></li>
+         <!-- n2 -->
+      </ul>
+   </li>
+   <li>
+      <a href="#">Bénéficiaires</a>
+      <ul>
+         <li><a href="AddClient.php"> Créer un Bénéficiaire </a></li>
+         <li><a href="SearchClt.php">Rechercher un Bénéficiaire</a></li>
+      </ul>
+   </li>
+   <li>
+      <a href="#">Facturation</a>
+      <ul>
+         <li><a href="#"> Créer une Facture </a></li>
+         <li><a href="#">Rechercher une Facture</a></li>
+      </ul>
+   </li>
+</ul>
 </header>
--- Developpement/index.php
+++ Developpement/index.php
@@ -5,32 +5,11 @@
     <title>Projet Communauté de Commune des Lisière de L'Oise</title>
     <link rel="stylesheet" type="text/css" href="css/reboot.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script src="js/responsiveslides.js"></script>
   </head>
   <body>
   <?php include 'Header.php' ?>
 
-  <script>
-      //Script du slider étant obligatoirement dans l'index.
-      $(function () {
-        $("#slider4").responsiveSlides({
-          auto: true,
-          pager: false,
-          nav: true,
-          speed: 500,
-          namespace: "callbacks",
-          before: function () {
-            $('.events').append("<li>before event fired.</li>");
-          },
-          after: function () {
-            $('.events').append("<li>after event fired.</li>");
-          }
-        });
-
-      });
-    </script>
+  
 
     <div class="text_slider">
         <p>Bienvenue sur le site de la Communauté de Commue des Lisière de L'Oise! <br>
